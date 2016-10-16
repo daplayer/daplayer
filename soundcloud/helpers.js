944b2c6084a21183f405fce694b2380e591e7715
Robin Dupret <robin.dupret@gmail.com>
2016-10-16 21:49:53 +0200

    Use our new placeholder API for activities translations
    We are only supporting English and French at the moment so this is not a
    problem but if we are going to support other locales, the user's name
    may not be at the right position to describe activities.

--- soundcloud/helpers.js
+++ soundcloud/helpers.js
@@ -18,9 +18,10 @@ Handlebars.registerHelper('soundcloud_activity_icon', function(type) {
 });
 
 Handlebars.registerHelper('soundcloud_activity_desc', function(origin, type) {
-  var key  = 'sc.activities.' + type;
-  var desc = `${origin.username} ${Translation.t(key)}`;
   var img  = Html.tag('img', {src: origin.avatar_url.size('tiny')});
+  var desc = Translation.t('sc.activities.' + type, {
+    user: origin.username
+  });
 
   return new Handlebars.SafeString(img + desc);
 });
