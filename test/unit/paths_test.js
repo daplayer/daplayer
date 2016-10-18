b0a38de033c5eb1f0cd36b0490321280831081b1
Robin Dupret <robin.dupret@gmail.com>
2016-10-18 21:39:05 +0200

    Add some basic unit tests for the `Paths` class

---
+++ test/unit/paths_test.js
@@ -0,0 +1,58 @@
+require('../test_helper');
+
+describe('Paths', () => {
+  describe('#join', () => {
+    it('should join all the given strings', () => {
+      var result = Paths.join('/home/jacky', 'Musics', 'Cherokee - American Spirit', 'Imagine.mp3');
+
+      assert.equal(result, '/home/jacky/Musics/Cherokee - American Spirit/Imagine.mp3');
+    });
+  });
+
+  describe('#resolve', () => {
+    it('should delegate to the `path.resolve` method', () => {
+      var result = Paths.resolve(Paths.join(__dirname, '../unit'));
+
+      assert.equal(result, __dirname);
+    });
+  });
+
+  describe('getters', () => {
+    var key, value;
+
+    before(() => {
+      key   = process.platform == 'win32' ? 'USERPROFILE' : 'HOME';
+      value = process.env[key];
+
+      process.env[key] = 'foo';
+    });
+
+    after(() => {
+      process.env[key] = value;
+    });
+
+    it('should have the proper value for `user`', () => {
+      assert.equal(Paths.user, 'foo/.daplayer');
+    });
+
+    it('should have the proper value for `default_artwork`', () => {
+      assert.equal(Paths.default_artwork, 'assets/images/default_artwork.svg');
+    });
+
+    it('should have the proper value for `covers`', () => {
+      assert.equal(Paths.covers, 'foo/.daplayer/covers');
+    });
+
+    it('should have the proper value for `playlists`', () => {
+      assert.equal(Paths.playlists, 'foo/.daplayer/playlists');
+    });
+
+    it('should have the proper value for `listen_later`', () => {
+      assert.equal(Paths.listen_later, 'foo/.daplayer/playlists/listen-later.jspf');
+    });
+
+    it('should have the proper value for `youtube_history`', () => {
+      assert.equal(Paths.youtube_history, 'foo/.daplayer/playlists/youtube-history.jspf');
+    });
+  });
+});
