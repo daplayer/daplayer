b1fb730c1bef1137175baec53f4adc5b06dbebed
Robin Dupret <robin.dupret@gmail.com>
2016-12-22 19:34:32 +0100

    Add Handlebars helpers for I18n#{plural,title}ize
    Add Handlebars helpers to delegate to the different I18n inflection
    methods and also remove some dead tests for the application helpers.

--- app/helpers.js
+++ app/helpers.js
@@ -3,3 +3,11 @@
 Handlebars.registerHelper('options', function(scope) {
   return new Handlebars.SafeString(Html.options(scope.data.root.service, true));
 });
+
+Handlebars.registerHelper('pluralize', function(count, word) {
+  return I18n.pluralize(count, word);
+});
+
+Handlebars.registerHelper('titleize', function(word) {
+  return I18n.titleize(word);
+});
