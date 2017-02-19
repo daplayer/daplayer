daplayer/daplayer

commit 44e8c7f6fdb1f8d374a834df43f39d8fe3705a77

Avoid caching data inside promises
Since we only want to wrap data within a promise when it is accessed
from the model, let's rather store it roughly for easier reuses when
we know that the data is already computed.

The `fetch` method has been added as a convenient way to wrap cached
data inside a Promise object.

Thus, we can easily fetch the current context even for paginated
sections as picking the view's context would only give the new data
loading the next page.

youtube/model.js
@@ -6,7 +6,7 @@ const LocalModel = require('../local/models/playlists');
 module.exports = class YouTubeModel {
   static history() {
     if (Cache.youtube.history)
-      return Cache.youtube.history;
+      return Cache.fetch('youtube', 'history');
 
     return LocalModel.loadPlaylist(Paths.youtube_history).then((history) => {
       return Cache.add('youtube', 'history', history);
@@ -45,7 +45,7 @@ module.exports = class YouTubeModel {
     if (action == 'items' && Cache.youtube.items.includes(token_or_id))
       return this.findPlaylist(token_or_id);
     else if (Cache.youtube[action] && !token_or_id)
-      return Cache.youtube[action];
+      return Cache.fetch('youtube', 'action');
 
     return YT[action](token_or_id, full).then((set) => {
       return {
@@ -123,7 +123,7 @@ module.exports = class YouTubeModel {
    */
   static addToPlaylist(id, record) {
     if (Cache.youtube.items[id])
-      Cache.youtube.playlists.then((cached) => {
+      Cache.fetch('youtube', 'playlists').then((cached) => {
         var playlist = cached.collection.find(playlist => playlist.id == id);
 
         playlist.items.push(record);

app/base/controller.js
@@ -12,11 +12,11 @@ module.exports = class BaseController {
   static render(view, context, id) {
     var [module, action] = view.split('/');
 
-    if (!(context instanceof Artist) && !(context instanceof Array)
-        && !(context instanceof Playlist) && !context.collection)
-      var cached_context = new Context(context);
-    else
+    if (context instanceof Artist   || context instanceof Array ||
+        context instanceof Playlist || context.collection)
       var cached_context = context;
+    else
+      var cached_context = new Context(Cache[module][action]);
 
     // Define the current scope.
     Cache.current = {

app/cache.js
@@ -12,6 +12,7 @@ module.exports = class Cache {
     this.templates  = {}; // Compiled templates
     this.soundcloud = {}; // SoundCloud records
     this.local      = {}; // Local records
+    this.meta       = {}; // Meta records
     this.playing    = {}; // Playing scope
     this.search     = {}; // Searching scope
 
@@ -40,8 +41,7 @@ module.exports = class Cache {
   /**
    * Facility to add elements to a specific cache section.
    *
-   * When the section is empty, just store the result inside
-   * a Promise.
+   * When the section is empty, the given data is roughly stored.
    *
    * When the section is already filled, the existing collection
    * will be concatenated with the given one and the cursor to
@@ -51,7 +51,7 @@ module.exports = class Cache {
    *                            the root section.
    * @param  {String} section - The section to fill.
    * @param  {Object} data    - The data to store.
-   * @return {Promise}
+   * @return {Object}
    */
   static add(module, section, data) {
     // Early return for `video_urls` since we have a higher
@@ -67,26 +67,26 @@ module.exports = class Cache {
     // is to ensure that we erase existing data as we may
     // update the local library tagging some elements.
     if (!this[module][section] || module == 'local') {
-      return this[module][section] = Promise.resolve(data);
+      return this[module][section] = data;
     } else {
-      return this[module][section].then((existing) => {
-        return this[module][section] = new Promise((resolve) => {
-          // Sometimes the SoundCloud API gives a `next_href` that
-          // will return an empty collection, in this case we can't
-          // concatenate so let's return the existing one as is.
-          if (data.collection && data.collection.empty()) {
-            existing.next_token = null;
-            resolve(existing);
-          }
+      var existing       = this[module][section];
+      var new_collection = existing.collection.concat(data.collection);
 
-          var new_collection = existing.collection.concat(data.collection);
-
-          resolve({
-            next_token: data.next_token,
-            collection: new_collection
-          });
-        });
-      });
+      return this[module][section] = {
+        next_token: data.next_token,
+        collection: new_collection
+      };
     }
+  }
+
+  /**
+   * Wraps cache sections within a Promise object. This
+   * is a facility to return data inside a Promise since
+   * model methods return such kind of objects.
+   *
+   * @return {Promise}
+   */
+  static fetch(module, section) {
+    return Promise.resolve(this[module][section]);
   }
 };

app/models/context.js
@@ -8,6 +8,9 @@ module.exports = class Context {
     this.collection = [];
 
     Object.keys(hash).forEach((key) => {
+      if (key == 'next_token')
+        return;
+
       if (hash[key] && hash[key].collection) {
         hash[key].collection.forEach((el) => {
           if (el.items)

app/ui.js
@@ -164,10 +164,10 @@ module.exports = class Ui {
         action == 'playlist_items' || !Cache[module][action])
       return;
 
-    Cache[module][action].then((result) => {
-      if (result.next_token)
-        Ui.render([module, action].join('/'), result.next_token);
-    });
+    var cached = Cache[module][action];
+
+    if (cached.next_token)
+      Ui.render([module, action].join('/'), cached.next_token);
   }
 
   /**

local/models/files.js
@@ -3,7 +3,7 @@
 module.exports = class LocalModelFiles {
   static files(section) {
     if (Cache.local[section])
-      return Cache.local[section];
+      return Cache.fetch('local', section);
 
     return new Promise((resolve, reject) => {
       const cp    = require('child_process');

soundcloud/models/fetch.js
@@ -5,7 +5,7 @@ const SC = require('../client');
 module.exports = class SoundCloudModelFetch {
   static fetch(action, href, limit, cache_key) {
     if (Cache.soundcloud[cache_key || action] && !href)
-      return Cache.soundcloud[cache_key || action];
+      return Cache.fetch('soundcloud', (cache_key || action));
 
     if (href && action == 'activities')
       var offset = href;

test/models/app/context_test.js
@@ -34,5 +34,11 @@ describe('Context', () => {
 
       assert.deepEqual(context.collection, [1, 2, 3, 4, 5, 6]);
     });
+
+    it('should ignore the `next_token` key', () => {
+      var context = new Context({next_token: undefined});
+
+      assert.deepEqual(context.collection, []);
+    });
   });
 });

commit 03c8fab8da3c66fc0b6d53c05ad5eee108f681ce

Fix reading existing data from cache for the YouTube section
Regression introduced by 44e8c7f6.

youtube/model.js
@@ -45,7 +45,7 @@ module.exports = class YouTubeModel {
     if (action == 'items' && Cache.youtube.items.includes(token_or_id))
       return this.findPlaylist(token_or_id);
     else if (Cache.youtube[action] && !token_or_id)
-      return Cache.fetch('youtube', 'action');
+      return Cache.fetch('youtube', action)
 
     return YT[action](token_or_id, full).then((set) => {
       return {