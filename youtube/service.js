59ca503f30cfc5e8504dd4b956e4fe895ca12b47
Robin Dupret <robin.dupret@gmail.com>
2017-03-14 22:41:31 +0100

    Fix YouTube signature descrambling

--- youtube/service.js
+++ youtube/service.js
@@ -265,7 +265,7 @@ module.exports = class YouTubeService extends NetService {
           if (error)
             reject(error);
 
-          var fname = jscode.match(/\.set\("signature",(.*?)\(/)[1];
+          var fname = jscode.match(/\.set\("signature",([a-z]*)\(/)[1];
           var pos   = jscode.indexOf(fname + "=function");
           var fbody = '', protoype = '';
 
